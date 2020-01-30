c298cea889fd7acedd7843de2a38919274f70415
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-01-30 15:08:30 +0530

    Add msg read from inbox

--- FindMe/app/src/main/java/com/hipo/findme/MainActivity.kt
+++ FindMe/app/src/main/java/com/hipo/findme/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.hipo.findme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
+
+const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        readSms()
     }
-    
+
+    @SuppressLint("Recycle")
+    private fun readSms() {
+        val cursor: Cursor? = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null)
+        val messageArray = arrayListOf<String>()
+        if (cursor!!.moveToFirst()) { // must check the result to prevent exception
+            do {
+                val msgData: String = cursor.getString(12)
+
+                if(msgData.contains("HELLO", ignoreCase = false)) {
+                    Log.i(TAG, "Msg: $msgData")
+                    messageArray.add(msgData)
+                }
+                // use msgData
+            } while (cursor.moveToNext())
+
+        } else { // empty box, no SMS
+        }
+    }
 }
